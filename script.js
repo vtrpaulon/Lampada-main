04d039d81aa1c85853e22440471ddd519d9a1fb6
vtrpaulon <vtrpaulon@gmail.com>
2023-06-03 15:26:23 -0300

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+function desligar(){
+  document.getElementById('lampada').innerHTML = '<img src="lampadaApagada.jpg"/>';
+}
+function ligar(){
+  document.getElementById('lampada').innerHTML = '<img src="lampadaAcesa.jpg"/>';
+}
